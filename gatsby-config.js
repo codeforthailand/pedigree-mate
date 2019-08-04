42ff1ecdae039d6ba3e7f635a025b576dc87b67e
heytitle <pat.chormai@gmail.com>
2019-08-04 15:55:25 +0700

    update ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,11 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "'UA-48736618-7",
+      },
+    },
   ],
 }
