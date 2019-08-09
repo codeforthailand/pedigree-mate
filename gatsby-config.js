3bcda9ccb76ff7f4b09bb3e42dd5e5cc4f903d17
heytitle <pat.chormai@gmail.com>
2019-08-09 22:12:34 +0700

    update pathPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: `/pedigree-mating`,
+  pathPrefix: `/pedigree-mate`,
   siteMetadata: {
     title: `Pedigree Mating`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
